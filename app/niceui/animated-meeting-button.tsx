2e53271fdb003b2991360d329b97c9a7b2ceeb79
richhost <hexcy@pm.me>
2024-11-13 19:54:17 +0800

    update animated meeting button

--- app/niceui/animated-meeting-button.tsx
+++ app/niceui/animated-meeting-button.tsx
@@ -12,7 +12,7 @@ export default function AnimatedMeetingButton() {
       <div className="size-11 rounded-full bg-neutral-950 grid place-items-center">
         <ArrowRight size={20} />
       </div>
-      <div className="size-11 rounded-full bg-white text-neutral-950 grid place-items-center absolute right-1.5 transition-transform duration-300 ease-in-out rotate-45 translate-x-[calc(100%+0.375rem)] group-hover:translate-x-0 group-hover:rotate-0">
+      <div className="size-11 rounded-full bg-white text-neutral-950 grid place-items-center absolute right-1.5 transition-transform duration-500 ease-in-out rotate-90 translate-x-[calc(100%+0.375rem)] group-hover:translate-x-0 group-hover:rotate-0">
         <ArrowRight size={20} />
       </div>
     </button>
