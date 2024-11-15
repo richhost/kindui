377f5c58c0da4216770a1cca16b733d1212554e9
Abiee <hexcy@pm.me>
2024-11-15 20:17:56 +0800

    update preview bg

--- app/components/mdx/preview.tsx
+++ app/components/mdx/preview.tsx
@@ -3,35 +3,34 @@ import { cn } from "~/lib/utils";
 import { PREVIEW_COMPONENTS_MAPPING } from "~/shared/preview-component-mapping";
 
 interface PreviewProps extends React.HTMLAttributes<HTMLDivElement> {
-  name: string;
+	name: string;
 }
 
 export function Preview({ name, className, ...props }: PreviewProps) {
-  const Component = useMemo(() => {
-    return PREVIEW_COMPONENTS_MAPPING[name];
-  }, [name]);
+	const Component = useMemo(() => {
+		return PREVIEW_COMPONENTS_MAPPING[name];
+	}, [name]);
 
-  return (
-    <section
-      className={cn(
-        "relative min-h-[350px] border rounded-md flex items-center justify-center not-prose",
-        className
-      )}
-      {...props}
-    >
-      <div
-        className="absolute inset-0 pointer-events-none -z-10"
-        aria-hidden
-        style={{
-          opacity: 0.15,
-          backgroundImage:
-            "linear-gradient(#b1b1b1 0.4px, transparent 0.4px), linear-gradient(to right, #b1b1b1 0.4px, #ffffff 0.4px)",
-          backgroundSize: "8px 8px",
-        }}
-      ></div>
-      <Suspense>
-        <Component />
-      </Suspense>
-    </section>
-  );
+	return (
+		<section
+			className={cn(
+				"relative min-h-[350px] border rounded-md flex items-center justify-center not-prose",
+				className
+			)}
+			{...props}
+		>
+			<div
+				className="absolute inset-0 pointer-events-none -z-10"
+				aria-hidden
+				style={{
+					opacity: 0.6,
+					backgroundImage: "radial-gradient(#a2a2a2 0.5px, #ffffff 0.5px)",
+					backgroundSize: "10px 10px"
+				}}
+			></div>
+			<Suspense>
+				<Component />
+			</Suspense>
+		</section>
+	);
 }
